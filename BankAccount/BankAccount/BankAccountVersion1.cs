ed187c27fc1a4b16cd69f99ba9e6699309565ef5
MariusA <anca.ticau@gmail.com>
2019-07-01 00:44:19 +0300

    innitial commit- bankAccount

---
+++ BankAccount/BankAccount/BankAccountVersion1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    // Simulate a bank account supporting opening/closing, withdrawals, and deposits of money.
+
+    // It should be possible to close an account; operations against a closed account must fail.
+
+    // Remember you are working with money so you should use an appropriate data type for it.
+
+    public enum BankAccountStatus
+    {
+        Open,
+        Closed
+    }
+
+    public class BankAccountVersion1
+    {
+        private BankAccountOwner owner;
+        private decimal balance;
+
+        public BankAccountStatus Status { get; set; }
+
+        public BankAccountVersion1(BankAccountOwner owner)
+        {
+            this.owner = owner;
+            this.Status = BankAccountStatus.Closed;
+        }
+
+        public void Open()
+        {
+            if (Status == BankAccountStatus.Closed)
+            {
+                Status = BankAccountStatus.Open;
+                Console.WriteLine("The account has been opened.");
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (Status == BankAccountStatus.Closed)
+            {
+                Console.WriteLine($"The account is closed, {nameof(Deposit)} operation invalid.");
+                return;
+            }
+
+            balance += amount;
+            Console.WriteLine($"You have deposited '{amount}', current balance = '{balance}'.");
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Status == BankAccountStatus.Closed)
+            {
+                Console.WriteLine($"The account is closed, {nameof(Withdraw)} operation invalid.");
+                return;
+            }
+
+            if (amount > balance)
+            {
+                Console.WriteLine($"Insufficient funds, current balance = '{balance}'.");
+                return;
+            }
+
+            balance -= amount;
+            Console.WriteLine($"You have withdrawn '{amount}', current balance = '{balance}'.");
+        }
+
+        public void ShowBalance()
+        {
+            if (Status == BankAccountStatus.Closed)
+            {
+                Console.WriteLine($"The account is closed, {nameof(ShowBalance)} operation invalid.");
+                return;
+            }
+
+            Console.WriteLine($"Current balance = '{balance}'.");
+        }
+
+        public void ShowOwner()
+        {
+            if (owner == null)
+            {
+                Console.WriteLine("The account doesn't have an owner.");
+                return;
+            }
+
+            Console.WriteLine($"Account owner = '{owner.Id}'.");
+        }
+    }
+
+    public abstract class BankAccountOwner
+    {
+        public string Id { get; set; }
+    }
+
+    public class Person : BankAccountOwner
+    {
+        private string firstName;
+        private string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.Id = firstName + " " + lastName;
+        }
+    }
+
+    public class Company : BankAccountOwner
+    {
+        private string name;
+
+        public Company(string name)
+        {
+            this.name = name;
+            this.Id = name;
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John", "Smith");
+            Company company1 = new Company("Microsoft");
+            BankAccountVersion1 bankAccount1 = new BankAccountVersion1(person1);
+            BankAccountVersion1 bankAccount2 = new BankAccountVersion1(company1);
+
+            bankAccount1.Deposit(10);
+            bankAccount1.Withdraw(1000);
+            bankAccount1.Open();
+
+            bankAccount1.Deposit(20);
+            bankAccount1.Deposit(50);
+            bankAccount1.ShowBalance();
+
+            bankAccount1.Withdraw(1000);
+            bankAccount1.Withdraw(30);
+
+            bankAccount1.ShowBalance();
+
+            bankAccount1.ShowOwner();
+            bankAccount2.ShowOwner();
+
+            Console.ReadKey();
+        }
+    }
+}
